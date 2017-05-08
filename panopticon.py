642a570299b0c7df2ecc78792ab5a33e03c368b6
URANO Ryoya <urano.works.mail@gmail.com>
2017-05-08 23:33:37 +0900

    :sparkles: Flaskの設定をファイルに分離

--- panopticon.py
+++ panopticon.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from pathlib import Path
 
+
+project_root = Path('.')
 app = Flask(__name__)
+app.config.from_pyfile(project_root/'config'/'app.cfg')
+
 
 @app.route('/')
 def index():
@@ -8,5 +13,5 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=app.config['PORT'], host=app.config['HOST'])
 
